88a1d0ebbe67924b63361c67ffff489c420dcb3c
nestasia1 <109359981+nestasia1@users.noreply.github.com>
2023-04-05 01:39:00 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -5,4 +5,4 @@ import numpy as np
 chat_id = 1022895883 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array) -> float:
-    return 2 / 67**2 * x.median()
+    return 2 / 67**2 * np.median(x)
